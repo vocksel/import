a9bfe2f2c9a275d3c1840c63e97d08d7ed7c3ec2
David Minnerly <voxeldavid@gmail.com>
2021-08-11 12:06:24 -0400

    Add some more tests

--- src/createImporter.spec.lua
+++ src/createImporter.spec.lua
@@ -4,6 +4,28 @@ return function()
 
 	local MOCK_MODULE = script.Parent.mocks.tableModule
 
+	it("should error for invalid argument types", function()
+		expect(function()
+			createImporter("bad", Instance.new("Folder"))
+		end).to.throw()
+
+		expect(function()
+			createImporter(Instance.new("Folder"), "bad")
+		end).to.throw()
+
+		expect(function()
+			createImporter()
+		end).to.throw()
+	end)
+
+	it("should error when given an invalid option", function()
+		expect(function()
+			createImporter(Instance.new("Folder"), Instance.new("Folder"), {
+				badOption = true,
+			})
+		end).to.throw()
+	end)
+
 	it("should support custom aliases", function()
 		local start = Instance.new("Script")
 		local child = Instance.new("Folder")
@@ -23,9 +45,15 @@ return function()
 			alias = tree.nest1.nest2.alias,
 		}
 
-		local import = createImporter(tree, start, aliases)
+		local import = createImporter(tree, start, {
+			aliases = aliases,
+		})
 
 		expect(import("alias/child")).to.equal(child)
+	end)
+
+	it("should allow the use of WaitForChild", function()
+		-- TODO: Implement an option to enable WaitForChild
 	end)
 
 	it("should throw when deteching a require loop", function()
