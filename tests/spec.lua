566d65685203adcfd02dc430586ad40d80f62b07
vocksel <voxeldavid@gmail.com>
2021-12-17 22:11:43 -0800

    Restructure test runner (#27)

--- tests/spec.lua
+++ tests/spec.lua
@@ -2,8 +2,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
 
-TestEZ.TestBootstrap:run({
-	ReplicatedStorage.import,
-})
+TestEZ.TestBootstrap:run({ ReplicatedStorage })
 
 return nil
