ba42fb49e6f9ee9f640e2e987d031b259524acc0
David Minnerly <voxeldavid@gmail.com>
2021-12-19 15:24:51 -0800

    Fix test runner again

--- tests/init.server.lua
+++ tests/init.server.lua
@@ -1,1 +1,13 @@
-require(script.spec)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local TestEZ = require(ReplicatedStorage.Package.TestEZ)
+
+local results = TestEZ.TestBootstrap:run({
+	ReplicatedStorage,
+})
+
+if results.failureCount > 0 then
+	print("❌ Test run failed")
+else
+	print("✔️ All tests passed")
+end
--- tests/spec.lua
+++
@@ -1,7 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local TestEZ = require(ReplicatedStorage.Packages.TestEZ)
-
-TestEZ.TestBootstrap:run({ ReplicatedStorage })
-
-return nil
