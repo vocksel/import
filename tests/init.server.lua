51df70bb3d28f4708bbb292037ebee2a75475d23
vocksel <voxeldavid@gmail.com>
2021-12-19 15:34:31 -0800

    Fix installation through Wally (#28)
    * Fix installation through Wally
    
    * Bump version
    
    * Remove wally.lock from the repo
    
    * Mount packages
    
    * Update CI to latest version in other packages
    
    * Fix path to TestEZ
    
    * Fix test runner again
    
    * I can't spell

--- tests/init.server.lua
+++ tests/init.server.lua
@@ -1,1 +1,13 @@
-require(script.spec)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local TestEZ = require(ReplicatedStorage.Packages.TestEZ)
+
+local results = TestEZ.TestBootstrap:run({
+	ReplicatedStorage,
+})
+
+if results.failureCount > 0 then
+	print("❌ Test run failed")
+else
+	print("✔️ All tests passed")
+end
--- tests/spec.lua
+++
@@ -1,7 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
-
-TestEZ.TestBootstrap:run({ ReplicatedStorage })
-
-return nil
