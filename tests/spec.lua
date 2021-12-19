ba15a10c26b9dea9c3302b7b4dbc5abd9135aac5
David Minnerly <voxeldavid@gmail.com>
2021-12-19 15:19:47 -0800

    Fix path to TestEZ

--- tests/spec.lua
+++ tests/spec.lua
@@ -1,6 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
+local TestEZ = require(ReplicatedStorage.Packages.TestEZ)
 
 TestEZ.TestBootstrap:run({ ReplicatedStorage })
 
