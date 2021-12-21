c545b576fc3bbbee6790aad46822d6393418788b
David Minnerly <me@vocksel.com>
2021-12-20 18:20:17 -0800

    Another test case

--- src/getInstanceFromFullName.spec.lua
+++ src/getInstanceFromFullName.spec.lua
@@ -45,6 +45,21 @@ return function()
 		expect(path).to.equal(module)
 	end)
 
+	it("should find spec files BEFORE the module it is associated with", function()
+		local module = Instance.new("ModuleScript")
+
+		folder = newFolder({
+			foo = newFolder({
+				bar = Instance.new("ModuleScript"),
+				["bar.spec"] = module,
+			}),
+		})
+		folder.Parent = ReplicatedStorage
+
+		local path = getInstanceFromFullName(module:GetFullName())
+		expect(path).to.equal(module)
+	end)
+
 	it("should return nil if the first part of the path is not a service", function()
 		expect(getInstanceFromFullName("Part")).to.never.be.ok()
 	end)
