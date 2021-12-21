0d69ee2afc334f85593e296f79c61ac6e609183d
David Minnerly <me@vocksel.com>
2021-12-20 17:49:27 -0800

    Return the actual caller

--- src/getCallingScript.lua
+++ src/getCallingScript.lua
@@ -1,28 +1,19 @@
 local getInstanceFromFullName = require(script.Parent.getInstanceFromFullName)
 
-local function getCallingScript(): BaseScript?
+local function getCallingScript(module: ModuleScript): LuaSourceContainer
 	local level = 1
+	local foundModule = false
 
 	while true do
-		local callerName = debug.info(level, "s")
+		local callerPath = debug.info(level, "s")
+		local nextCaller = getInstanceFromFullName(callerPath)
 
-		-- debug.info() will return nil when the level is out of scope of the
-		-- traceback, so we can exit out at this point if we didn't find a
-		-- calling script.
-		--
-		-- Fun fact: It might be impossible for this block to run. If
-		-- debug.info() returns nil, that means we were not able to find a
-		-- calling Script in this loop. But then how else would this function
-		-- run if a Script did not call it? Maybe this condition could be
-		-- satisfied in CI, but who knows!
-		if not callerName then
-			return nil
-		end
-
-		local caller = getInstanceFromFullName(callerName)
-
-		if caller and caller:IsA("BaseScript") then
-			return caller
+		if nextCaller == module then
+			foundModule = true
+		else
+			if foundModule then
+				return nextCaller
+			end
 		end
 
 		level += 1
--- src/getCallingScript.spec.lua
+++ src/getCallingScript.spec.lua
@@ -1,12 +1,8 @@
 return function()
-	local ServerScriptService = game:GetService("ServerScriptService")
-
 	local getCallingScript = require(script.Parent.getCallingScript)
 
-	local TEST_RUNNER = ServerScriptService.tests
-
-	it("returns the Script instance that called the function", function()
-		local caller = getCallingScript()
-		expect(caller).to.equal(TEST_RUNNER)
+	it("returns the LuaSourceContainer that called the function", function()
+		local caller = getCallingScript(script)
+		expect(caller:IsA("LuaSourceContainer")).to.equal(true)
 	end)
 end
