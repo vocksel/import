155d6bbe869a72f87e7b2f0fdbf78a1c3f01fd59
David Minnerly <me@vocksel.com>
2023-01-08 12:22:56 -0800

    Add `analyze` job

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -3,7 +3,7 @@
 curl -s -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/master/scripts/globalTypes.d.lua
 
 cp .github/workflows/.luaurc Packages
-rojo sourcemap default.project.json -o sourcemap.json
+rojo sourcemap dev.project.json -o sourcemap.json
 
 luau-lsp analyze --sourcemap=sourcemap.json --defs=globalTypes.d.lua --defs=testez.d.lua src/
 
