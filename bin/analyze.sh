08fe90c186aea1e40060a18136205e06a4a18188
David Minnerly <me@vocksel.com>
2023-01-08 12:22:15 -0800

    Ignore packages in analysis

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -2,8 +2,10 @@
 
 curl -s -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/master/scripts/globalTypes.d.lua
 
-rojo sourcemap -o sourcemap.json
+cp .github/workflows/.luaurc Packages
+rojo sourcemap default.project.json -o sourcemap.json
 
 luau-lsp analyze --sourcemap=sourcemap.json --defs=globalTypes.d.lua --defs=testez.d.lua src/
 
+rm Packages/.luaurc
 rm globalTypes.d.lua
